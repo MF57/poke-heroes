3ad5734a1df15a103a673aa89c23797e333a76ea
MF57 <p-bochenek@outlook.com>
2017-09-10 18:48:05 +0200

    Camera

---
+++ Assets/_Scripts/CameraController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public int speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		if(Input.GetKey(KeyCode.RightArrow) && transform.position.x <= 25)
+		{
+			transform.Translate(new Vector3(speed * Time.deltaTime,0,0));
+		}
+		if(Input.GetKey(KeyCode.LeftArrow) && transform.position.x >= -25)
+		{
+			transform.Translate(new Vector3(-speed * Time.deltaTime,0,0));
+		}
+		if(Input.GetKey(KeyCode.DownArrow) && transform.position.z >= -30)
+		{
+			transform.Translate(new Vector3(0, -speed * Time.deltaTime * 0.6f , -speed * Time.deltaTime));
+		}
+		if(Input.GetKey(KeyCode.UpArrow) && transform.position.z <= -5)
+		{
+			transform.Translate(new Vector3(0, speed * Time.deltaTime * 0.6f, speed * Time.deltaTime));
+		}
+	}
+}
